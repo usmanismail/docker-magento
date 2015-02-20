13772b9ce29167f2662850c8448c108f3e0802da
Usman Ismail <0xfffffff@gmail.com>
2015-02-20 11:51:24 -0500

    Do not rely on env variables from containers

--- start.sh
+++ start.sh
@@ -13,12 +13,12 @@ sed -i "s/<password\/>/<password>password<\/password>/g" /var/www/app/etc/config
 
 echo "Creating the magento database..."
 
-echo "create database magento" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h db -P "$DB_PORT_3306_TCP_PORT"
+echo "create database magento" | mysql -u "$DB_USER" --password="$DB_PASS" -h db -P "$DB_PORT_3306_TCP_PORT"
 
 while [ $? -ne 0 ]; do
 	sleep 5
-        echo "create database magento" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h db -P "$DB_PORT_3306_TCP_PORT"
-        echo "show tables" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h db -P "$DB_PORT_3306_TCP_PORT" magento
+        echo "create database magento" | mysql -u "$DB_USER" --password="$DB_PASS" -h db -P "$DB_PORT_3306_TCP_PORT"
+        echo "show tables" | mysql -u "$DB_USER" --password="$DB_PASS" -h db -P "$DB_PORT_3306_TCP_PORT" magento
 done
 
 echo "Adding Magento Caching"
