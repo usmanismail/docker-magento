51a993151d4b97d2885557c05b431257b452c7b1
Usman Ismail <0xfffffff@gmail.com>
2015-02-20 15:03:29 -0500

    Updating to use precreated database from MYSQL container

--- start.sh
+++ start.sh
@@ -2,30 +2,23 @@
 
 if [[ -e /firstrun ]]; then
 
-echo "not first run so skipping initialization"
+	echo "Not first run so skipping initialization"
 
 else 
 
-echo "setting the default installer info for magento"
-sed -i "s/<host>localhost/<host>db/g" /var/www/app/etc/config.xml
-sed -i "s/<username\/>/<username>user<\/username>/" /var/www/app/etc/config.xml
-sed -i "s/<password\/>/<password>password<\/password>/g" /var/www/app/etc/config.xml
+	echo "setting the default installer info for magento"
+	sed -i "s/<host>localhost/<host>db/g" /var/www/app/etc/config.xml
+	sed -i "s/<username\/>/<username>user<\/username>/" /var/www/app/etc/config.xml
+	sed -i "s/<password\/>/<password>password<\/password>/g" /var/www/app/etc/config.xml
 
-echo "Creating the magento database..."
+	echo "show tables" | mysql -u "$DB_ENV_MYSQL_USER" --password="$DB_ENV_MYSQL_PASSWORD" -h db -P "$DB_PORT_3306_TCP_PORT" magento
 
-echo "create database magento" | mysql -u "$DB_USER" --password="$DB_PASS" -h db -P "$DB_PORT_3306_TCP_PORT"
 
-while [ $? -ne 0 ]; do
-	sleep 5
-        echo "create database magento" | mysql -u "$DB_USER" --password="$DB_PASS" -h db -P "$DB_PORT_3306_TCP_PORT"
-        echo "show tables" | mysql -u "$DB_USER" --password="$DB_PASS" -h db -P "$DB_PORT_3306_TCP_PORT" magento
-done
+	echo "Adding Magento Caching"
 
-echo "Adding Magento Caching"
+	sed -i -e  '/<\/config>/{ r /var/www/app/etc/mage-cache.xml' -e 'd}' /var/www/app/etc/local.xml.template
 
-sed -i -e  '/<\/config>/{ r /var/www/app/etc/mage-cache.xml' -e 'd}' /var/www/app/etc/local.xml.template
-
-touch /firstrun
+	touch /firstrun
 
 fi
 
