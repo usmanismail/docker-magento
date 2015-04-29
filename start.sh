24667f4c7ac5f1dff1ab76809bcec30457c84dd0
Usman Ismail <usman@techtraits.com>
2015-04-28 21:49:36 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,16 +7,13 @@ if [[ -e /firstrun ]]; then
 else 
 
 	echo "setting the default installer info for magento"
-	sed -i "s/<host>localhost/<host>${DB_PORT_3306_TCP_ADDR}/g" /var/www/app/etc/config.xml
+	sed -i "s/<host>localhost/<host>${MYSQL_HOST}/g" /var/www/app/etc/config.xml
 	sed -i "s/<username\/>/<username>user<\/username>/" /var/www/app/etc/config.xml
 	sed -i "s/<password\/>/<password>password<\/password>/g" /var/www/app/etc/config.xml
 
-	echo "show tables" | mysql -u "$DB_ENV_MYSQL_USER" --password="$DB_ENV_MYSQL_PASSWORD" -h $DB_PORT_3306_TCP_ADDR -P "$DB_PORT_3306_TCP_PORT" magento
-
-
 	echo "Adding Magento Caching"
 
-        echo "$CACHE_PORT_11211_TCP_ADDR	cache" >> /etc/hosts
+        echo "$MEMCACHED_HOST	cache" >> /etc/hosts
 	sed -i -e  '/<\/config>/{ r /var/www/app/etc/mage-cache.xml' -e 'd}' /var/www/app/etc/local.xml.template
 
 	touch /firstrun
